74b322bd0df69f210f5c6b5f4a11a66346166a64
ANDES <andresmontealegregiraldo@gmail.com>
2018-05-22 16:16:36 -0500

    Menu superior paso 1

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,8 +9,37 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request)    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/nosotros", name="nosotros")
+     */
+    public function nosotrosAction(Request $request)    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/productos", name="productos")
+     */
+    public function productosAction(Request $request)    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/reservas", name="reservas")
+     */
+    public function reservasAction(Request $request)    {
         // replace this example code with whatever you need
         return $this->render('frontal/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
